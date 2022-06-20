00ae5478471596a43f2000c2cfab3e5f728292b7
Jenifer Irwin <jenifer.irwin@gmail.com>
2022-06-19 22:59:11 -0400

    Increased max level to 100

--- src/merc.h
+++ src/merc.h
@@ -144,7 +144,7 @@ typedef void SPELL_FUN args( ( int sn, int level, CHAR_DATA *ch, void *vo,
 #define MAX_PC_RACE        6
 #define MAX_CLAN           3
 #define MAX_DAMAGE_MESSAGE 41
-#define MAX_LEVEL          60
+#define MAX_LEVEL          100
 #define LEVEL_HERO         (MAX_LEVEL - 9)
 #define LEVEL_IMMORTAL     (MAX_LEVEL - 8)
 #define L_IMM LEVEL_IMMORTAL
